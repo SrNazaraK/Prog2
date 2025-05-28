d8dd10ec6b88fc70fd884f8a98d608eb5013332e
SrNazaraK <santiagocarrasquero@gmail.com>
2025-05-28 00:00:43 -0400

    funcion para diferenciar mayus y minus

--- PRACTICA1.cpp
+++ PRACTICA1.cpp
@@ -2,36 +2,45 @@
 #include <fstream>
 #include <string>
 #include <locale.h>
+#include <cctype>
 
 using namespace std;
 
+void minusculas(char* cadena){
+	for (char* p = cadena; *p; p++){
+		*p = tolower(*p);
+	}
+}
+
 int main(){	
-	system("chcp 65001 > nul");
+
 	setlocale(LC_ALL, "");
 
     ifstream archivo("archivo.txt");
 
-    if (archivo.is_open()) {
-        string linea;
-        while (getline(archivo, linea)){ 
+    if (!archivo.is_open()) {
+			cout << "No se pudo abrir el archivo para su lectura." << endl;
         }
         archivo.close();
-    }
-    else {
-        cout << "No se pudo abrir el archivo para lectura" << endl;
-    } 
     
     string busqueda;
     
+	cout << "Este es un programa que te permite leer un archivo de texto linea por linea." << endl;
+    
     do {
-    	cout << "Este es un programa que te permite leer un archivo de texto linea por linea." << endl;
-    	cout << "Ingresa la palabra o caracter que deseas buscar: "; 
-    	getline(cin, busqueda);
+    	do {
+    		cout << "Ingresa la palabra o caracter que deseas buscar: "; 
+    		getline(cin, busqueda);
     	
-    	if (palabra.empty()){
-    		cout << "No puede estar vacio. Intentalo de nuevo" << endl;
-		}
-	} while (busqueda.empty());
+    		if (busqueda.empty()){
+    			cout << "No puede estar vacio. Intentalo de nuevo" << endl;
+			}
+		} while (busqueda.empty());
+		
+		string linea;
+		int coincidencias = 0;
 
+	} while (opcion == 'S');
+	
     return 0;
 }
