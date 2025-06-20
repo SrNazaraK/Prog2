b4fea1b47904b64bf078b12db244f7a6d0863e66
SrNazaraK <santiagocarrasquero@gmail.com>
2025-06-19 21:33:03 -0400

    funciones para mostrar por categoria, buscar, modificar y eliminar. Y en el main, se agregaron los demas casos al switch

--- binario.cpp
+++ binario.cpp
@@ -43,8 +43,53 @@ void mostrar(bool solo_activos = true) {
     ifstream f("inventario.dat", ios::binary);
     for (Producto p; f.read((char*)&p, sizeof(p));)
         if (!solo_activos || p.activo)
-            cout << "Codigo: " << p.codigo << ", Nombre: " << p.nombre << ", Precio: " << p.precio << ", Stock: " << p.stock << ", Categoria: " << p.categoria;
+            cout << "Codigo: " << p.codigo << ", Nombre: " << p.nombre << ", Precio: " << p.precio << ", Stock: " << p.stock << ", Categoria: " << p.categoria << "\n";
 }
+
+void mostrarPorCategoria() {
+    char cat[40]; cout << "Categoria: "; cin.ignore(); cin.getline(cat, 40);
+    fstream f("inventario.dat", ios::in | ios::binary);
+    Producto p;
+    while (f.read((char*)&p, sizeof(p)))
+        if (p.activo && strcmp(p.categoria, cat) == 0)
+            cout << p.codigo << ": " << p.nombre << "\n";
+}
+
+void buscar() {
+    int c; cout << "Codigo: "; cin >> c;
+    fstream f("inventario.dat", ios::in | ios::binary);
+    Producto p;
+    f.seekg(pos(c));
+    if (f.read((char*)&p, sizeof(p)) && p.activo)
+        cout << p.nombre << ", $" << p.precio << ", stock: " << p.stock
+             << ", categoria: " << p.categoria << "\n";
+    else cout << "No encontrado.\n";
+}
+
+void modificar() {
+    int c; cout << "Codigo: "; cin >> c;
+    fstream f("inventario.dat", ios::in | ios::out | ios::binary);
+    Producto p;
+    f.seekg(pos(c));
+    if (f.read((char*)&p, sizeof(p)) && p.activo) {
+        cout << "Nuevo precio: "; cin >> p.precio;
+        cout << "Nuevo stock: "; cin >> p.stock; cin.ignore();
+        cout << "Nueva categoria: "; cin.getline(p.categoria, 40);
+        f.seekp(pos(c)); f.write((char*)&p, sizeof(p));
+    } else cout << "No encontrado.\n";
+}
+
+void eliminar() {
+    int c; cout << "Codigo: "; cin >> c;
+    fstream f("inventario.dat", ios::in | ios::out | ios::binary);
+    Producto p;
+    f.seekg(pos(c));
+    if (f.read((char*)&p, sizeof(p)) && p.activo) {
+        p.activo = false;
+        f.seekp(pos(c)); f.write((char*)&p, sizeof(p));
+    } else cout << "No encontrado.\n";
+}
+
 int main() {
     crearArchivo();
     int op;
@@ -54,6 +99,10 @@ int main() {
         switch(op) {
             case 1: agregar(); break;
             case 2: mostrar(); break;
+            case 3: mostrarPorCategoria(); break;
+            case 4: buscar(); break;
+            case 5: modificar(); break;
+            case 6: eliminar(); break;
         }
     } while (op != 0);
 }
