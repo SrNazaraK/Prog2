af24fdfc0b9f74da6fd7098e942f581bdde2583a
SrNazaraK <santiagocarrasquero@gmail.com>
2025-06-19 20:01:49 -0400

    acceso aleatorio y funcion para crear archivo binario

--- binario.cpp
+++ binario.cpp
@@ -7,3 +7,11 @@ struct Producto {
     int codigo; char nombre[31], categoria[40];
     float precio; int stock; bool activo;
 };
+
+long pos(int c) { return (c - 1) * sizeof(Producto); }
+
+void crearArchivo() {
+    fstream f("inventario.dat", ios::in | ios::binary);
+    if (!f) f.open("inventario.dat", ios::out | ios::binary);
+}
+
