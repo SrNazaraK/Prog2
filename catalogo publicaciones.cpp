58e10e401c35af6fc59de4fe1c7145aa96d5d9a1
SrNazaraK <santiagocarrasquero@gmail.com>
2025-07-02 01:12:08 -0400

    clases publicacion, libro, revista, periodico. objetos.

---
+++ catalogo publicaciones.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+class Publicacion {
+protected:
+    string titulo;
+    int ano;
+public:
+    Publicacion(string t, int a) : titulo(move(t)), ano(a) {}
+    virtual void mostrar() const = 0;
+    virtual string tipo() const = 0;
+    virtual ~Publicacion() {}
+    string getTitulo() const { return titulo; }
+    int getAno() const { return ano; }
+};
+
+class Libro : public Publicacion {
+    int paginas;
+public:
+    Libro(string t, int a, int p) : Publicacion(move(t), a), paginas(p) {}
+    void mostrar() const override {
+        cout << "[Libro] \"" << titulo << "\" - Año: " << ano << ", Páginas: " << paginas << '\n';
+    }
+    string tipo() const override { return "Libro"; }
+};
+
+class Revista : public Publicacion {
+    int edicion;
+public:
+    Revista(string t, int a, int e) : Publicacion(move(t), a), edicion(e) {}
+    void mostrar() const override {
+        cout << "[Revista] \"" << titulo << "\" - Año: " << ano << ", Edición: " << edicion << '\n';
+    }
+    string tipo() const override { return "Revista"; }
+};
+
+class Periodico : public Publicacion {
+    string fecha;
+public:
+    Periodico(string t, int a, string f) : Publicacion(move(t), a), fecha(move(f)) {}
+    void mostrar() const override {
+        cout << "[Periódico] \"" << titulo << "\" - Año: " << ano << ", Fecha: " << fecha << '\n';
+    }
+    string tipo() const override { return "Periodico"; }
+};
+
